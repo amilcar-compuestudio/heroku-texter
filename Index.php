35aaae3dc4fa3d896e181ea41cf394beb505dd65
Amilcar Gómez <amilkargomez@hotmail.es>
2021-04-21 09:08:00 -0500

    show err

--- Index.php
+++ Index.php
@@ -1,3 +1,7 @@
 <?php
-            require_once "paginas/inicio.php";
-        ?>
+
+			ini_set('display_errors', 1);
+			ini_set('display_startup_errors', 1);
+			error_reporting(E_ALL);
+            require_once __DIR__."/paginas/inicio.php";
+?>
